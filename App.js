64e8e316ac1636a4c5db4f2e6eac5c0d06f195e0
Sol Battaglia <solbattaglia@gmail.com>
2020-11-15 12:30:26 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+
+import * as React from 'react';
+import HomeScreen from './src/components/HomeScreen'
+import DetailsScreen from './src/components/DetatilScreen'
+import { createStackNavigator } from '@react-navigation/stack';
+import {LogBox, StatusBar} from 'react-native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+
+
+
+const Stack = createStackNavigator();
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#E3350D',
+    normal: '#6D6D4E',
+    fire: '#F08030',
+    fighting: '#C03028',
+    water: '#6890F0',
+    flying: '#A890F0',
+    grass: '#78C850',
+    poison: '#A040A0',
+    electric: '#F8D030',
+    ground: '#E0C068',
+    psychic: '#F85888',
+    rock: '#B8A038',
+    ice: '#98D8D8',
+    bug: '#A8B820',
+    dragon: '#7038F8',
+    ghost: '#705898',
+    dark: '#705848',
+    steel: '#B8B8D0',
+    fairy: '#EE99AC',
   },
-});
+};
+
+const App = () => (
+  <>
+  
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor={MyTheme.colors.primary}
+    />
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: MyTheme.colors.primary,
+          },
+          headerTintColor: '#fff',
+        }}>
+        <Stack.Screen name="Pokédex" component={HomeScreen} />
+        <Stack.Screen name="Pokémon" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </>
+);
+
+export default App;
